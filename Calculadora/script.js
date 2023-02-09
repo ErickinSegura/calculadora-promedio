09f9ab5bb091c5b5b5a78d207a640cad8a75f553
ErickinSegura <segsanerick@gmail.com>
2023-02-08 19:59:38 -0600

    Calculadora Añadida

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,9 +1,17 @@
 function calcular() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-    var resultado = document.getElementById("resultado");
+  var blq1 = parseFloat(document.getElementById("blq1").value);
+  var blq2 = parseFloat(document.getElementById("blq2").value);
+  var blq3 = parseFloat(document.getElementById("blq3").value);
 
-    
-      resultado.value = num1 + num2 / 2;
-    
-  }
+  var UDF1 = parseFloat(document.getElementById("UDF1").value);
+  var UDF2 = parseFloat(document.getElementById("UDF2").value);
+  var UDF3 = parseFloat(document.getElementById("UDF3").value);
+  var UDF4 = parseFloat(document.getElementById("UDF4").value);
+
+  var opta = parseFloat(document.getElementById("opta").value);
+
+
+
+  var resultado = document.getElementById("resultado");
+  resultado.value = ((blq1*3)+(blq2*3)+(blq3*3)+(UDF1)+(UDF2)+(UDF3*2)+(UDF4*2)+(opta*3)) / 18;
+}
